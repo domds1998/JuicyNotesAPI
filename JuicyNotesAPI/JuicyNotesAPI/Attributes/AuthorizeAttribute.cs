d8bf1b6c689ee0363e4b23ae1c6fcb5156de7d1d
Jakub Szczepański <s18636@pjwstk.edu.pl>
2020-12-12 04:23:30 +0100

    add jwtbearer
    v 3.1.6

--- JuicyNotesAPI/JuicyNotesAPI/Attributes/AuthorizeAttribute.cs
+++ JuicyNotesAPI/JuicyNotesAPI/Attributes/AuthorizeAttribute.cs
@@ -14,8 +14,8 @@ namespace JuicyNotesAPI.Attributes
     {
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            var person = (User)context.HttpContext.Items["User"];
-            if (person == null)
+            var user = (User)context.HttpContext.Items["User"];
+            if (user == null)
             {
                 // not logged in
                 context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
